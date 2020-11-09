a52e7eb729bd76d5531404300293eda04c56feaf
viniciusmdias <vinicius-mdias@hotmail.com>
2020-11-09 18:01:08 -0300

    Create project with initial template

---
+++ README.md
@@ -0,0 +1,27 @@
+# Trabalho A.7 Programação Paralela
+
+Alunos: Vinicius Dias (15103263) e Arthur Mota ()
+
+## Introdução
+
+Nesta atividade, precisamos desenvolver duas versões para um programa que conta palavras em um texto.
+
+Dado um texto e um conjunto de "palavras chave", foi criado um programa sequencial e um programa paralelo (utilizando OpenMP) para contabilizar o número de ocorrências de cada palavra.
+
+Variamos o tamanho do texto e o número de threads, para fazer uma avaliação do speed-up e da eficiência do programa paralelo.
+
+## Como executar o código
+
+Temos dois códigos nesse projeto, a versão sequencial e a versão paralela com OpenMP.
+
+### Código sequencial
+
+### Código paralelo
+
+Ir ao diretório do projeto pelo terminal e executar o seguinte comando para criar o programa:
+
+## Detalhes sobre a implementação das soluções e dos exemplos testados e avaliação do desempenho mostrando o speed-up e a eficiência
+
+## Conclusão
+
+## Referências
---
+++ paralelo/countWordsParallel.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define TAM_TEXTO 750
+#define TAM_LINHA 50
+
+char palavras_chave[4][5] = {"to", "for", "fly", "night"};
+int ocorrencias_palavra_chave[4];
+
+char texto[] = "Blackbird singing in the dead of night \
+                Take these broken wings and learn to fly\
+                All your life You were only waiting for this\
+                moment to arise Blackbird singing in the\
+                dead of night Take these sunken eyes and\
+                learn to see All your life You were only\
+                waiting for this moment to be free Blackbird,\
+                fly, blackbird, fly to the light of the dark\
+                black night Blackbird, fly, blackbird, fly\
+                Into the light of the dark black night Blackbird\
+                singing in the dead of night Take these broken\
+                wings and learn to fly All your life\
+                You were only waiting for this moment to arise\
+                You were only waiting for this moment to arise\
+                You were only waiting for this moment to arise\0";
+
+int num_ocorrencias(char *linha, char *palavra) {
+  int cont = 0;
+  /* contabiliza o número de ocorrência de uma palavra */
+  return cont;
+}
+
+int main(){
+  /* pesquisa linha por linha pela ocorrência de cada palavra chave
+  * e atribuiu o número de ocorrências em cada posição do vetor
+  * ocorrencias_palavra_chave[] */
+  
+}
