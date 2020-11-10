a0ed139c79b60256577abfb6875023fa5f3e194f
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-11-10 18:14:25 -0300

    If applied this commit will add directive for openmp

--- paralelo/contadorDePalavras.c
+++ paralelo/contadorDePalavras.c
@@ -141,18 +141,21 @@ char texto[] = "Blackbird singing in the dead of night\
               You were only waiting for this moment to arise\
               You were only waiting for this moment to arise\0";
 
-char palavras_chave[4][5] = {"to", "for", "fly", "night"};
+char palavras_chaves[4][5] = {"to", "for", "fly", "night"};
 int tamanho_do_texto, tamanho_da_palavra_chave, count, i, j, x;
 
 int main () {
-  int total_de_palavras_chave = sizeof(palavras_chave)/sizeof(palavras_chave[0]);
-  printf("Selecionamos um total de %d palavras chaves \n\n", total_de_palavras_chave);
+  tamanho_do_texto = strlen(texto);
+
+  int total_de_palavras_chaves = sizeof(palavras_chaves)/sizeof(palavras_chaves[0]);
+  printf("Selecionamos um total de %d palavras chaves \n\n", total_de_palavras_chaves);
 
   gettimeofday(&t1, NULL);
-
-  for(x = 0; x < total_de_palavras_chave; x++) {
-    num_ocorrencias(palavras_chave[x]);
-  }
+        
+    #pragma omp parallel for num_threads(total_de_palavras_chaves)
+    for (int x = 0; x < total_de_palavras_chaves; x++) {
+      num_ocorrencias(palavras_chaves[x]);
+    }
 
   gettimeofday(&t2, NULL);
   
@@ -160,13 +163,11 @@ int main () {
   printf("O tempo que está aplicação levou para executar foi de: %f\n", t_total);
 }
 
-
 int num_ocorrencias(char palavra_chave[]) {
-  tamanho_do_texto = strlen(texto);
+  
   tamanho_da_palavra_chave = strlen(palavra_chave);
-  i = j = count = 0;
 
-  while (i < tamanho_do_texto) {
+  for (i = 0; i < tamanho_do_texto; i++) {
     if (texto[i] == palavra_chave[0]) {
       // first letter of the word found
       for (j = 0; j < tamanho_da_palavra_chave; j++) {
@@ -179,7 +180,7 @@ int num_ocorrencias(char palavra_chave[]) {
       if (j == tamanho_da_palavra_chave) {
         // match found
         if (isspace(texto[i + j]) || ispunct(texto[i + j]) || texto[i + j] == '\0') {
-          count++;
+          count ++;
           i += j;
         }
       }
@@ -187,7 +188,6 @@ int num_ocorrencias(char palavra_chave[]) {
     else {
       proximaPalavra();
     }
-    i++;
   }
 
   printf("Palavra chave: %s\n", palavra_chave);
